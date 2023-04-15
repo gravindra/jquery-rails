fea7826dcbed893a1bdf53860270a77de35f5526
Ravindra Anandrao Ghare <elite-rg@Ravindras-MacBook-Air.local>
2023-04-15 15:11:39 +0530

    updated gem details

--- vendor/assets/javascripts/jquery.js
+++ vendor/assets/javascripts/jquery.js
@@ -1,14 +1,6 @@
 /*!
  * http://jquery.com/
  *
- * Includes Sizzle.js
- * http://sizzlejs.com/
- *
- * Copyright jQuery Foundation and other contributors
- * Released under the MIT license
- * http://jquery.org/license
- *
- * Date: 2016-05-20T17:17Z
  */
 
 (function( global, factory ) {
